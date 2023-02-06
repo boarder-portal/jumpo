dd24ce25f19b4277bff204c87074f94e081b911e
droooney <d.vor7@mail.ru>
2023-02-07 00:44:46 +0600

    side movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,17 +3,29 @@ using UnityEngine;
 namespace Player {
   public class PlayerController : MonoBehaviour {
     [SerializeField] private float jumpForce = 15f;
+    [SerializeField] private float sideForce = 10f;
 
     private Rigidbody2D _rigidbody;
 
-    void Start() {
+    private void Start() {
       _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    void Update() {
-      if (Input.GetKeyDown("space")) {
-        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpForce);
+    private void Update() {
+      var newVelocityX = 0f;
+      var newVelocityY = _rigidbody.velocity.y;
+
+      if (Input.GetKey("a")) {
+        newVelocityX = -sideForce;
+      } else if (Input.GetKey("d")) {
+        newVelocityX = sideForce;
       }
+
+      if (Input.GetKeyDown("space")) {
+        newVelocityY = jumpForce;
+      }
+
+      _rigidbody.velocity = new Vector2(newVelocityX, newVelocityY);
     }
   }
 }
