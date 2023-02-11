4609f39e8e77a36108fcb9eea28b2142b1ec390c
droooney <d.vor7@mail.ru>
2023-02-11 20:42:20 +0600

    apples counter text

--- Assets/Scripts/Player/AppleCollector.cs
+++ Assets/Scripts/Player/AppleCollector.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
+using TMPro;
 
 namespace Player {
   public class AppleCollector : MonoBehaviour {
+    [SerializeField] private TextMeshProUGUI counterText;
+
+    private int _applesCount;
+
     private void OnTriggerEnter2D(Collider2D collision) {
       if (collision.gameObject.CompareTag("Apple")) {
         Destroy(collision.gameObject);
+
+        _applesCount++;
+
+        counterText.text = $"{_applesCount}";
       }
     }
   }
